02011384c414ce0f21be4d762a9fe36e5f58a0c4
JY16 <JY16@users.noreply.github.com>
2017-09-30 12:18:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-// Dropdown/toggle Navigation
+// Mobile Navigation
 
-var toggleMenu = document.querySelectorAll(".menu-toggle")[0];
-toggleMenu.addEventListener("click", menu, false);
-function menu() {
-	document.querySelector("body").classList.toggle("open");
+function mobileNav() {
+    let menuToggle = document.querySelector(".menu-toggle");
+    let nav = document.querySelector(".mobile-nav");
+
+    menuToggle.addEventListener("click", function() {
+        if (nav.style.display === "flex") {
+            nav.style.display = "none";
+        }
+        else {
+            nav.style.display = "flex";
+        }
+    });
 }
 
+mobileNav();
